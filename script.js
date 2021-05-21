7671c6756bad1164d9a00a3f715f4be4a14f06b0
Mike Dawicki <76268149+mikedawicki09@users.noreply.github.com>
2021-05-21 12:40:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 alert, confirm, console, prompt
 */
  /*jslint devel: true */
+
+/*function to toggle the menu bar*/
 function navbarToggle() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -9,4 +11,19 @@ function navbarToggle() {
   } else {
     x.className = "topnav";
   }
+}
+
+/*function to open the skill tabs*/
+function openTab(page, elmnt) {
+    var i, tabContent, selectedTab;
+    /*grabs all elements that have the class tabContent*/
+    tabContent = document.getElementsByClassName("tabContent");
+    /*loops through each of the selected elements and hides them*/
+    for(i =0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    
+    selectedTab = document.getElementById(page).style.display = "block";
+
+
 }
