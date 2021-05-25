566b29d5b5b08afa8d6aa0191a83a16252e72f1d
Mike Dawicki <76268149+mikedawicki09@users.noreply.github.com>
2021-05-25 14:42:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,12 @@ function fade(page) {
     //        fade.style.opacity = opacity;
     //    }
     //}, 80); 
-
-
-
+}
+function scrollToTop() {
+    var position =
+        document.body.scrollTop || document.documentElement.scrollTop;
+ if (position) {
+        window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
+        scrollAnimation = setTimeout("scrollToTop()", 30);
+    } else clearTimeout(scrollAnimation);
 }
