748c9a7199c6dbe99068c0142964215eeb04d47d
Mike Dawicki <76268149+mikedawicki09@users.noreply.github.com>
2021-06-04 14:15:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
-/*function to toggle the menu bar*/
-function navbarToggle() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
 
 /*function to open the skill tabs*/
-function openTab(page, elmnt) {
-    var i, tabContent, selectedTab;
+function openTab(page, elmnt, skillType) {
+    var i, tabContent;
     /*grabs all elements that have the class tabContent*/
     tabContent = document.getElementsByClassName("tabContent");
+    var slider = document.getElementById(skillType);
     /*loops through each of the selected elements and hides them*/
     for (i = 0; i < tabContent.length; i++) {
         tabContent[i].style.display = "none";
     }
     var tabButton = document.getElementsByClassName("tabButton");
     for (i = 0; i < tabButton.length; i++) {
-        tabButton[i].style.backgroundColor = "#557A95";
+        tabButton[i].style.backgroundColor = "#1f2833";
+        tabButton[i].style.color = "white";
         tabButton[i].style.borderTop = "0px";
     }
-
 
     document.getElementById(page).style.display = "block";
     elmnt.style.backgroundColor = "white";
     elmnt.style.color = "black";
     elmnt.style.border = "2px solid lightgray";
     elmnt.style.borderBottom = "0px";
-    elmnt.style.borderTop = "3px solid #557A95";
+    elmnt.style.borderTop = "3px solid #1f2833";
+
+    count = 0;
+
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    var navigation = document.querySelector("#nav");
+    if (prevScrollpos > currentScrollPos) {
+        navigation.style.display = "block";
+    }
+    else {
+        navigation.style.display = "none";
+    }
+    prevScrollpos = currentScrollPos;
 }
 
 /*function to remove tab content*/
@@ -37,28 +45,83 @@ function cancelTab(page, elmnt) {
     var cancelTab = document.getElementById(page).style.display = "none";
     var tabButton = document.getElementsByClassName("tabButton");
     for (i = 0; i < tabButton.length; i++) {
-        tabButton[i].style.backgroundColor = "#557A95";
+        tabButton[i].style.backgroundColor = "#1f2833";
+        tabButton[i].style.color = "white";
         tabButton[i].style.borderTop = "0px";
     }
 }
 
 
-function fade(page) {
-    var opacity = 0;
-    var fade = document.getElementById(page);
-    fade.style.display = 'block';
-    //var timer = setInterval(function () {
-    //    if (opacity < 1) {
-    //        opacity += 0.1;
-    //        fade.style.opacity = opacity;
-    //    }
-    //}, 80); 
+var count = 0;
+function sliderScript(num, skillType) {
+    var listItem = document.getElementsByClassName(skillType);
+    count += num;
+    for (var i of listItem) {
+        if (count == 0) {
+            i.style.left = "0%";
+        }
+        if (count == 1) {
+            if (skillType == 'frameSkill') {
+                i.style.left = "-35%";
+                count = 1;
+            }
+            else {
+                i.style.left = "-75%";
+            }
+        }
+        if (count == 2) {
+            if (skillType == 'frameSkill') {
+                i.style.left = "-35%";
+                count = 1;
+            }
+            else {
+                i.style.left = "-170%";
+            }
+        }
+        if (count > 2) {
+            if (skillType == 'frameSkill') {
+                i.style.left = "-35%";
+                count = 0;
+            }
+            else {
+                i.style.left = "-170%";
+                count = 2;
+            }
+        }
+        if (count < 0) {
+            i.style.left = "0%";
+            count = 0;
+        }
+    }
+
 }
-function scrollToTop() {
-    var position =
-        document.body.scrollTop || document.documentElement.scrollTop;
- if (position) {
-        window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
-        scrollAnimation = setTimeout("scrollToTop()", 30);
-    } else clearTimeout(scrollAnimation);
+
+$(document).ready(function () {
+    var scrollLink = $('.scroll');
+    scrollLink.click(function (e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1200)
+    })
+})
+
+
+function showNav() {
+    const navbarList = document.querySelector(".my-nav-list");
+    const menuBtn = document.querySelector(".menu-btn");
+    navbarList.classList.add("active");
+    menuBtn.classList.add("hide");
 }
+function removeNav() {
+    const navbarList = document.querySelector(".my-nav-list");
+    const menuBtn = document.querySelector(".menu-btn");
+    navbarList.classList.remove("active");
+    menuBtn.classList.remove("hide");
+}
+
+
+
+
+
+
